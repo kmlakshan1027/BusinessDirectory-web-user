cd2f02d1d8910a4e44cf9e92c148724e1d0ab214
Lakshan <lakshankm1027@gmail.com>
2025-11-14 16:42:07 +0530

    New comitt

--- src/pages/User/Login.jsx
+++ src/pages/User/Login.jsx
@@ -192,7 +192,7 @@ const Login = () => {
             }} 
           />
           <h1 className="login-title">Welcome</h1>
-          <p className="login-subtitle">Please sign in to your account using your registered E-mail</p>
+          <p className="login-subtitle">Please sign in to your account using your registered E-mail & Password</p>
           
           {error && <div className="error-message">{error}</div>}
           
